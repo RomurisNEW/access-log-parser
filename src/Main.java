31c1989ae7714e17b0a83ac8a08bd65c27688a6c
Roman Grishin <grishin655@gmail.com>
2025-12-13 01:21:01 +0300

    Написана программа с бекоснемным циклом while, которая принимает путь до существующего файла

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Введите первое число: ");
-        int number1 = new Scanner(System.in).nextInt();
-        System.out.print("Введите второе число: ");
-        int number2 = new Scanner(System.in).nextInt();
+        int countPath = 0;
+        while (true) {
+            System.out.println("Укажите путь к файлу без кавычек в начале и конце: ");
+            String path = new Scanner(System.in).nextLine();
 
-        int x = number1;
-        int y = number2;
-        System.out.println("Сумма: "+(x+y));
-        System.out.println("Разность: "+(x-y));
-        System.out.println("Произведение: "+(x*y));
-        System.out.println("Частное: "+((double)x/(double)y));
+            File file = new File(path);
+            boolean fileExist = file.exists();
+
+            boolean isDirectory = file.isDirectory();
+
+            if (isDirectory) {
+                System.out.println("Указанный путь ведет к папке, а не файлу");
+                continue;
+            }
+
+            if (fileExist) {
+                System.out.print("Файл существует. ");
+            } else {
+                System.out.println("По данному пути нет файла");
+                continue;
+            }
+
+            System.out.println("Путь указан верно");
+            countPath++;
+            System.out.println("Это файл номер: " + countPath);
+        }
     }
 }
