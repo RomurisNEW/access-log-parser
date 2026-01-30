4277a9d30b593c8f9ecde9ce97b29b223bf179e2
Roman Grishin <grishin655@gmail.com>
2026-01-30 18:48:17 +0300

    Добавлены метод indexOf для поиска строки, substring для получения строки и split для отделения. Решение задачи 2 курсового проекта - Сравнение объектов

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int countPath = 0;
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Укажите путь к файлу без кавычек в начале и конце: ");
-            String path = new Scanner(System.in).nextLine();
+            String path = scanner.nextLine();
+
 
             File file = new File(path);
             boolean fileExist = file.exists();
@@ -25,7 +27,7 @@ public class Main {
             if (fileExist) {
                 System.out.print("Файл существует. ");
             } else {
-                System.out.println("По данному пути нет файла");
+                System.out.println("Файла не существует");
                 continue;
             }
 
@@ -35,38 +37,57 @@ public class Main {
 
 
             int countLine = 0;
-            int maxLengthLine = Integer.MIN_VALUE;
-            int minLengthLine = Integer.MAX_VALUE;
+            int yandexBotCount = 0;
+            int googleBotCount = 0;
             try {
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader =
                         new BufferedReader(fileReader);
                 String line;
 
+
                 while ((line = reader.readLine()) != null) {
                     int length = line.length();
                     countLine++;
-                    if (length > maxLengthLine) {
-                        maxLengthLine = length;
+
+                    int start = line.indexOf("(compatible");
+
+                    if (start == -1) {
+                        continue;
                     }
-                    if (length < minLengthLine) {
-                        minLengthLine = length;
+
+                    String rightName = "";
+                    String firstBrackets = line.substring(start + 1);
+                    String[] parts = firstBrackets.split(";");
+                    if (parts.length >= 2) {
+                        String fragment = parts[1];
+                        rightName = fragment.split("/")[0].trim();
                     }
+
+                    if (rightName.equals("YandexBot")){
+                        yandexBotCount++;
+                    } else if (rightName.equals("Googlebot")){
+                        googleBotCount++;
+                    }
+
+//                    if(line.contains("YandexBot")){
+//                        yandexBotCount++;
+//                    } else if (line.contains("Googlebot")){
+//                        googleBotCount++;
+//                    }
 
                     if (length > 1024) {
                         throw new CastomException("Кол-во символов в строке не может быть больше 1024 символов");
                     }
-
-//                    if (length > 1024) {
-//                        throw new RuntimeException("Кол-во символов в строке не может быть больше 1024 символов");
-//                    }
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
             System.out.println("Общее кол-во строк в файле: " + countLine);
-            System.out.println("Максимальная длина строки в файле: " + maxLengthLine);
-            System.out.println("Минимальная длина строки в файле: " + minLengthLine);
+            System.out.println("Кол-во YandexBot встречаемых в первых скобках: " + yandexBotCount + ". Доля запросов " +
+                    "от YandexBot = " + String.format("%.3f", (double) yandexBotCount/countLine));
+            System.out.println("Кол-во Googlebot встречаемых в первых скобках: " + googleBotCount + ". Доля запросов " +
+                    "от Googlebot = " + String.format("%.3f", (double) googleBotCount/countLine));
         }
     }
 }
